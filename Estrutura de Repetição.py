24bcc8d6d01acc3f19434e3b339e3cc1da7b1b98
EwertonRod <ewertonrodrigues230@gmail.com>
2018-10-16 11:05:42 -0300

    2modificacao

--- Estrutura de Repetição.py
+++ Estrutura de Repetição.py
@@ -1,6 +1,23 @@
 idade = int(input("Digite sua idade"))
 resp = idade >=18
 
-if resp == True:
+if (idade >=18 ) == True:
     print("Pode dirigir")
     print(resp)
+
+    
+if resp == False:
+    print("você não pode dirigir")
+    print(resp)
+
+    
+""" outras formas de comparar  
+if resp :
+    print("você pode dirigir")
+    print(resp)
+
+
+if idade >=18 :
+    print("Pode dirigir")
+    print(resp)
+    """
