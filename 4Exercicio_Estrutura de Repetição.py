f669340332aa6387129d499b6be1812801a34d20
EwertonRod <ewertonrodrigues230@gmail.com>
2018-10-16 14:36:37 -0300

    5modificacao_Exercicio4

---
+++ 4Exercicio_Estrutura de Repetição.py
@@ -0,0 +1,14 @@
+
+print("\t\tPrograma Beneficio\n")
+
+
+
+idade= float(input("Digite sua idade "))
+
+
+
+if 18 <= idade < 70 :
+    print("você pode receber o beneficio")    
+
+else:
+    print("Você não tem direito ao beneficio")
