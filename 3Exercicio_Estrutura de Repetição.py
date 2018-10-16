1506ee3e716e44c2953c0f0db122405d6a536ff2
EwertonRod <ewertonrodrigues230@gmail.com>
2018-10-16 14:02:19 -0300

    5modificacao_Exercicio3

---
+++ 3Exercicio_Estrutura de Repetição.py
@@ -0,0 +1,15 @@
+"""
+Faça um programa que peç um valor e mostre na tela se o valor é positivo ou negativo"""
+print("\t\Identifica se o valor é positivo ou negativo\n")
+
+
+
+Valor= float(input("Digite um número: "))
+
+
+
+if Valor < 0 :
+    print("Esse numero é negativo")    
+
+else:
+    print("Esse número é positivo")
