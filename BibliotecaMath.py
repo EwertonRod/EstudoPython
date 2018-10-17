64195bb4f852637286600cb88bda23495bc7c49a
Ewerton <ewertonrodrigues230@gmail.com>
2018-10-17 16:41:34 -0300

    Teste

---
+++ BibliotecaMath.py
@@ -0,0 +1,7 @@
+import math
+
+print(math.sin(1))#pega um numero em radianos e devolve como senoo
+print(math.cos(2))#pega um numero em radianos e devolve como cosseno
+print(math.tan(3))#pega um numero em radianos e devolve como tangente
+print(math.pow(2,2))#pega um numero em radianos e devolve como potencia
+print(math.sqrt(4)) # pega um numero em radianos e devolve como raiz quadrada
