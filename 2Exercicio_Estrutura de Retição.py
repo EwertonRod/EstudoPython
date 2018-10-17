e71fecb3b88561b5fea969bce7bb1b400e5a05a8
EwertonRod <ewertonrodrigues230@gmail.com>
2018-10-17 10:55:24 -0300

    1Atualização_Repetição

---
+++ 2Exercicio_Estrutura de Retição.py
@@ -0,0 +1,7 @@
+""" imprimi o quadrado do numeros digitados até que seja 0"""
+
+n = int(input("Digite o primeiro numero"))
+while n!=0 :
+        print(n,"ao quadrado =", n*n)
+        n = int(input("digite o próximo numero:"))
+        
